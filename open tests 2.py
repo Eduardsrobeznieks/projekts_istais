eba104df8b34c4e6fef0af5118029c4fdd6fc30a
Eduardsrobeznieks <98820241+Eduardsrobeznieks@users.noreply.github.com>
2022-03-29 18:39:47 +0300

    Update open tests 2.py

--- open tests 2.py
+++ open tests 2.py
@@ -1,14 +1,14 @@
 keys=[]
-f = open('vardi1.csv', 'r')
+f = open('C:/Users/erobs/Documents/GitHub/projekts_istais/open tests/vardi2.csv', 'r', encoding='utf-8')
 for s in f:
     red_s = s.strip()
     keys.append(red_s)
 f.close()
 values=[]
-f = open('numuri1.csv', 'r')
+f = open('C:/Users/erobs/Documents/GitHub/projekts_istais/open tests/numuri1.csv', 'r')
 for s in f:
     red_s = s.strip()
     values.append(red_s)
 f.close()
-vardi = dict(zip(keys, values))
+vardi = dict(zip(keys , values))
 print(vardi)
